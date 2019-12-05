5db47b4bc1003a6ecb07acd80aa350883d48b940
Natalia Bryzhatenko <notika@Natalias-MBP.home>
2019-12-05 12:12:39 -0500

    open the first window, proceed to game

--- app/src/main/java/com/example/android17/MainActivity.java
+++ app/src/main/java/com/example/android17/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android17;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,9 +8,12 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button newGame;
+    private Button replayGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,23 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        newGame = findViewById(R.id.newGame);
+        replayGame = findViewById(R.id.replayGame);
+        newGame.setOnClickListener((V)->startNewGame());
     }
 
+
+    private void startNewGame() {
+//        Bundle bundle = new Bundle();
+
+//        bundle.putInt(AddEditMovie.MOVIE_INDEX, pos);
+//        bundle.putString(AddEditMovie.MOVIE_NAME, movie.name);
+//        bundle.putString(AddEditMovie.MOVIE_YEAR, movie.year);
+//        bundle.putString(AddEditMovie.MOVIE_DIRECTOR, movie.director);
+//        Intent intent = new Intent(this, AddEditMovie.class);
+//        intent.putExtras(bundle);
+//        startActivityForResult(intent, EDIT_MOVIE_CODE);
+          Intent intent = new Intent(this, PlayGame.class);
+          startActivity(intent);
+    }
 }
