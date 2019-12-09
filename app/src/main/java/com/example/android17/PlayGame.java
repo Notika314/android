2b486a488781bfeb718d2a4ed49c3786f0f3aaa4
notika0314 <nb631@scarletmail.rutgers.edu>
2019-12-09 00:57:59 -0500

    account config

--- app/src/main/java/com/example/android17/PlayGame.java
+++ app/src/main/java/com/example/android17/PlayGame.java
@@ -185,6 +185,7 @@ public class PlayGame extends AppCompatActivity implements OnItemClickListener {
             view.setBackgroundColor(0x00000000);
         }
     }
+    //configuring account
 
 }
 //notes:
